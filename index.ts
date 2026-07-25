f05c1400616afc4196d1156879218f4aeec0a854
tinman097 <146140811+tinman097@users.noreply.github.com>
2026-07-25 13:54:37 -0400

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 class CustomStreamProvider implements OnlineStreamProvider {
     
     async search(query: string): Promise<AnimeProvider_Anime[]> {
-        const response = await IndividualRequest.get(`http://100.89.97.87:8000/search?q=${encodeURIComponent(query)}`);
+        const response = await IndividualRequest.get(`http://100.89.97.87:8000/search?query=${encodeURIComponent(query)}`);
         const data = response.json();
         
         return data.map((item: any) => ({
@@ -24,7 +24,7 @@ class CustomStreamProvider implements OnlineStreamProvider {
         }));
     }
 
-   async findEpisodeServer(episodeId: any): Promise<AnimeProvider_Video[]> {
+    async findEpisodeServer(episodeId: any): Promise<AnimeProvider_Video[]> {
         const targetUrl = typeof episodeId === 'object' ? episodeId.url || episodeId.id || JSON.stringify(episodeId) : episodeId;
 
         const response = await IndividualRequest.get(`http://100.89.97.87:8000/servers?url=${encodeURIComponent(targetUrl)}`);
